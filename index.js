f76be19537b1af14afded6c6a0cf9b27331fac9b
Fletch S <stampsfj04@gmail.com>
2021-02-05 12:34:39 -0500

    server up and running

---
+++ index.js
@@ -0,0 +1,28 @@
+// $1 Eviroment varibles - dotenv, process.env
+
+// $2 Imports
+const express = require('express')
+const app = express()
+const ejslayouts = require('express-ejs-layouts')
+
+// $3 Middleware
+app.set('view engine', 'ejs')
+app.use(ejslayouts)
+app.use(() => {
+    console.log('Hello there...')
+})
+app.use(express.static(__dirname+'/public'));
+
+
+// $4 Routes
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+// $5 Controllers
+
+// Listen
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+    console.log(`server listening on PORT: ${PORT}`)
+})
